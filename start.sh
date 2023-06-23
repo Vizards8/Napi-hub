ecbaa8aa586074e36003a1f03b614b1b26b422cf
NengZhou <Vizards8@outlook.com>
2023-06-23 17:28:22 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-nohup sh /app/nacos/bin/startup.sh -m standalone &
+echo "Starting Nacos..."
+nohup sh /app/nacos/bin/startup.sh -m standalone >nacos.out 2>&1 &
 
-nohup java -jar /app/napi-interface-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
-nohup java -jar /app/napi-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
-nohup java -jar /app/napi-hub-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
+echo "Starting napi-interface..."
+nohup java -jar /app/napi-interface-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-interface.out 2>&1 &
 
+echo "Starting napi-gateway..."
+nohup java -jar /app/napi-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-gateway.out 2>&1 &
+
+echo "Starting napi-hub..."
+nohup java -jar /app/napi-hub-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-hub.out 2>&1 &
+
+echo "Starting napi-hub-frontend"
 cd /app/napi-hub-frontend
-nohup yarn start &
+nohup yarn start >napi-hub-frontend.out 2>&1 &
