88d92127c4b99be9a6fd94ebff974162924078f2
NengZhou <Vizards8@outlook.com>
2023-06-22 21:03:55 -0400

    Update

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ java -jar /app/napi-hub-backend/target/napi-hub-0.0.1-SNAPSHOT.jar --spring.prof
 java -jar /app/napi-gateway/target/napi-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
 java -jar /app/napi-interface/target/napi-interface-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
 
-sh /app/nacos-server-2.2.3/bin/startup.sh -m standalone
+sh /app/nacos-server-2.2.3/nacos/bin/startup.sh -m standalone
