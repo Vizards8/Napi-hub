3c39cc1418583ecc05384231a4508cfec876fa70
NengZhou <Vizards8@outlook.com>
2023-06-23 01:10:17 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sh /app/nacos/bin/startup.sh -m standalone
+nohup sh /app/nacos/bin/startup.sh -m standalone &
 
-java -jar /app/napi-interface-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod
-java -jar /app/napi-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod
-java -jar /app/napi-hub-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod
+nohup java -jar /app/napi-interface-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
+nohup java -jar /app/napi-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
+nohup java -jar /app/napi-hub-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
 
 cd /app/napi-hub-frontend
-yarn start
+nohup yarn start &
