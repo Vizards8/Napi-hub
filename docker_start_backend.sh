a12c3fc8f36270c7313a83d474a6bdfd0bafffdc
NengZhou <Vizards8@outlook.com>
2023-06-25 17:15:00 -0400

    Update

---
+++ docker_start_backend.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Starting napi-interface..."
+nohup java -jar /app/napi-interface/target/napi-interface-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-interface.out 2>&1 &
+sleep 10
+echo "Complete!"
+
+echo "Starting napi-hub..."
+nohup java -jar /app/napi-hub-backend/target/napi-hub-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-hub.out 2>&1 &
+sleep 40
+echo "Complete!"
+
+echo "Starting napi-gateway..."
+nohup java -jar /app/napi-gateway/target/napi-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-gateway.out 2>&1 &
+sleep 40
+echo "Complete!"
+
+echo "All projects have been started!"
