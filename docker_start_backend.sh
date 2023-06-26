53f98bcb80b632d6a67c1b77c8e0fe5b95ccd261
NengZhou <Vizards8@outlook.com>
2023-06-25 20:44:58 -0400

    Add docker compose

--- docker_start_backend.sh
+++ docker_start_backend.sh
@@ -6,12 +6,12 @@ sleep 10
 echo "Complete!"
 
 echo "Starting napi-hub..."
-nohup java -jar /app/napi-hub-backend/target/napi-hub-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-hub.out 2>&1 &
+nohup java -jar  -Xms256m -Xmx256m -Xmn128m  /app/napi-hub-backend/target/napi-hub-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-hub.out 2>&1 &
 sleep 40
 echo "Complete!"
 
 echo "Starting napi-gateway..."
-nohup java -jar /app/napi-gateway/target/napi-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-gateway.out 2>&1 &
+nohup java -jar  -Xms256m -Xmx256m -Xmn128m /app/napi-gateway/target/napi-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-gateway.out 2>&1 &
 sleep 40
 echo "Complete!"
 
