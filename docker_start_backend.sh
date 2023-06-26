24ac4d71e514f069c7b61d38eeeaba14d18f6b9c
NengZhou <Vizards8@outlook.com>
2023-06-25 23:14:11 -0400

    Update

--- docker_start_backend.sh
+++ docker_start_backend.sh
@@ -10,9 +10,9 @@ nohup java -jar  -Xms256m -Xmx256m -Xmn128m  /app/napi-hub-backend/target/napi-h
 sleep 40
 echo "Complete!"
 
-echo "Starting napi-gateway..."
-nohup java -jar  -Xms256m -Xmx256m -Xmn128m /app/napi-gateway/target/napi-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-gateway.out 2>&1 &
-sleep 40
-echo "Complete!"
+# echo "Starting napi-gateway..."
+# nohup java -jar  -Xms256m -Xmx256m -Xmn128m /app/napi-gateway/target/napi-gateway-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod >napi-gateway.out 2>&1 &
+# sleep 40
+# echo "Complete!"
 
 echo "All projects have been started!"
